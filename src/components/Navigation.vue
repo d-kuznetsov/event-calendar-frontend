c576a3eedbafb7251449ac0efbbf50c304d6736c
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-06 17:22:20 +0200

    test navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="Navigation">
-    <button v-if="isLoggedIn">Logout</button>
-    <button v-else>Logout</button>
+    <button v-if="isLoggedIn" @click="handleLogoutClick">Logout</button>
+    <button v-else @click="handleLoginClick">Logout</button>
   </div>
 </template>
 <script>
 import { computed } from "vue";
+import { useRouter } from "vue-router";
 import { useStore } from "../store";
 
 export default {
   setup() {
     const store = useStore();
+    const router = useRouter();
     const isLoggedIn = computed(() => store.getters.isLoggedIn);
+
+    const handleLoginClick = () => {
+      const testUser = {
+        name: "Katya",
+        password: "12345",
+      };
+      store.dispatch("login", testUser);
+      router.push("/calendar");
+    };
+    const handleLogoutClick = () => {
+      store.dispatch("logout");
+      router.push("/login");
+    };
+
     return {
       isLoggedIn,
+      handleLoginClick,
+      handleLogoutClick,
     };
   },
 };
