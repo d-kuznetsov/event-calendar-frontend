afc97ef91223d18a27ce3242678e21d580eb8699
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-06 15:54:54 +0200

    move css to slyles folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./assets/reset.css";
-import "./assets/styles.css";
+import "./assets/styles/reset.css";
+import "./assets/styles/common.css";
 
 createApp(App).use(router).mount("#app");
