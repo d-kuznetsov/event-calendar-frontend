cca4960a9301af457e83e8d978b08cc68364dd88
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-07 10:11:38 +0200

    add navigation guards

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,48 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { store } from "./store";
 import Home from "./views/Home.vue";
 import Register from "./views/Register.vue";
 import Login from "./views/Login.vue";
 import Calendar from "./views/Calendar.vue";
 
+const isAuthenticated = () => {
+  return store.getters.isLoggedIn;
+};
+
 const routes = [
-  { path: "/", component: Home },
-  { path: "/register", component: Register },
-  { path: "/login", component: Login },
-  { path: "/calendar", component: Calendar },
+  {
+    path: "/",
+    component: Home,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: "/register",
+    component: Register,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: "/login",
+    component: Login,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: "/calendar",
+    component: Calendar,
+    meta: { requiresAuth: true },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth) {
+    isAuthenticated() ? next() : next("/login");
+  } else {
+    isAuthenticated() ? next("/calendar") : next();
+  }
 });
 
 export default router;
