22de3daf4d310495847d86d74ef12fe9d017f1c6
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-12 14:27:18 +0200

    make real http request

--- src/store/service.ts
+++ src/store/service.ts
@@ -1,24 +1,26 @@
-function register(userData: { name: string; email: string; password: string }) {
-  console.log(userData);
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        name: userData.name,
-        email: userData.email,
-      });
-    }, 1000);
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: "http://localhost:8080/",
+  timeout: 1000,
+});
+
+async function register(userData: {
+  name: string;
+  email: string;
+  password: string;
+}) {
+  const res = await httpClient.post("/register", {
+    ...userData,
   });
+  return res.data;
 }
 
-function login(credentials: { email: string; password: string }) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        name: "test",
-        email: credentials.email,
-      });
-    }, 1000);
+async function login(credentials: { email: string; password: string }) {
+  const res = await httpClient.post("/login", {
+    ...credentials,
   });
+  return res.data;
 }
 
 export default {
