2911020ec5cc42ae00d47f9fe6302269ef55f56e
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-14 23:07:56 +0200

    fetch events in setup

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Calendar</div>
+  <div>
+    Calendar
+    {{ events }}
+  </div>
 </template>
+
+<script lang="ts">
+import { computed } from "vue";
+import { useStore } from "../store";
+
+export default {
+  setup() {
+    const store = useStore();
+    const events = computed(() => store.state.events);
+    store.dispatch("fetchEvents");
+
+    return {
+      events,
+    };
+  },
+};
+</script>
