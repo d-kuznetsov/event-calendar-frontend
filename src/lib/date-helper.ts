a949951b3c48700143804636df4e34b2c74cba52
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-15 23:43:12 +0200

    create class Range

--- src/lib/date-helper.ts
+++ src/lib/date-helper.ts
@@ -13,3 +13,37 @@ export function getWeekPeriod(date: Date | string): [string, string] {
   endWeek.setDate(endWeek.getDate() + (7 - diff));
   return [getDateAsStr(startWeek), getDateAsStr(endWeek)];
 }
+
+export class Range {
+  readonly from: string;
+  readonly to: string;
+
+  constructor(from: string, to: string) {
+    this.from = from;
+    this.to = to;
+  }
+
+  [Symbol.iterator]() {
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        if (this.current <= this.last) {
+          return { done: false, value: this.step() };
+        } else {
+          return { done: true };
+        }
+      },
+
+      step() {
+        const currentDate = new Date(this.current);
+        let dayOfMonth = currentDate.getDate();
+        const nextDate = new Date(currentDate);
+        nextDate.setDate(++dayOfMonth);
+        this.current = getDateAsStr(nextDate);
+        return getDateAsStr(currentDate);
+      },
+    };
+  }
+}
