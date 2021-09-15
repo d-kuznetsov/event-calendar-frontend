f5ffd9704e484150271c97f4b88bf356378f7986
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-15 17:06:53 +0200

    create Week component

---
+++ src/components/Week.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Week
+    {{ weekEvents }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, PropType } from "vue";
+import { Event } from "../store/types";
+
+export default defineComponent({
+  props: {
+    events: {
+      type: Array as PropType<Event[]>,
+      required: true,
+    },
+    startWeek: {
+      type: String,
+      required: true,
+    },
+    endWeek: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const weekEvents = computed(() =>
+      props.events.filter((e) => {
+        return e.date >= props.startWeek && e.date <= props.endWeek;
+      })
+    );
+    return {
+      weekEvents,
+    };
+  },
+});
+</script>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
     Calendar
-    {{ events }}
+    <Week :events="events" :startWeek="period[0]" :endWeek="period[1]" />
   </div>
 </template>
 
 <script lang="ts">
 import { computed } from "vue";
 import { useStore } from "../store";
+import Week from "../components/Week.vue";
 
 export default {
+  components: {
+    Week,
+  },
   setup() {
     const store = useStore();
     const events = computed(() => store.state.events);
+    const period = computed(() => store.state.period);
     store.dispatch("fetchEvents");
-
     return {
       events,
+      period,
     };
   },
 };
