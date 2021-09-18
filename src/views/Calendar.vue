10c459ca5c595224616acd852a04a9133acb6b65
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-18 14:53:09 +0200

    use EventEditor

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -5,27 +5,43 @@
       :startWeek="period[0]"
       :endWeek="period[1]"
       class="Calendar__week"
+      @eventClick="handleEventClick"
     />
+    <EventEditor v-if="isEditorOpen" @click="handleCloseClick" />
   </div>
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "../store";
 import Week from "../components/Week.vue";
+import EventEditor from "../components/EventEditor.vue";
 
 export default {
   components: {
     Week,
+    EventEditor,
   },
   setup() {
     const store = useStore();
     const events = computed(() => store.state.events);
     const period = computed(() => store.state.period);
+    const isEditorOpen = ref(false);
     store.dispatch("fetchEvents");
+
+    const handleEventClick = (eventId: string) => {
+      isEditorOpen.value = true;
+    };
+    const handleCloseClick = () => {
+      isEditorOpen.value = false;
+    };
+
     return {
       events,
       period,
+      isEditorOpen,
+      handleEventClick,
+      handleCloseClick,
     };
   },
 };
