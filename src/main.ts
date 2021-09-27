9be03ed9006b180c5227756ff6f8c23bcd4370e1
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-27 11:24:39 +0200

    add oruga component library

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import router from "./router";
 import { store, key } from "./store";
 import "./assets/styles/reset.css";
 import "./assets/styles/common.css";
+// import Oruga from "@oruga-ui/oruga-next";
+import "@oruga-ui/oruga-next/dist/oruga.css";
 
 createApp(App).use(router).use(store, key).mount("#app");
