79c3755a6da449a5d861be49df978cb075cbeeda
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-28 14:16:52 +0200

    delete oruga css

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,5 @@ import router from "./router";
 import { store, key } from "./store";
 import "./assets/styles/reset.css";
 import "./assets/styles/common.css";
-// import Oruga from "@oruga-ui/oruga-next";
-import "@oruga-ui/oruga-next/dist/oruga.css";
 
 createApp(App).use(router).use(store, key).mount("#app");
